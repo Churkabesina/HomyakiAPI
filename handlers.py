aff18596b58157b64cb3dc9c7c7b27f623f46134
Churkabesina <ilyacshutov@yandex.ru>
2024-04-04 08:44:35 +0400

    fix

--- handlers.py
+++ handlers.py
@@ -2,16 +2,15 @@ import requests
 
 
 class Handlers:
-    def __init__(self, ulr_local_node):
-        self.ulr_local_node = ulr_local_node
-        self.base_data = {
-            'id': 1,
-            'jsonrpc': '2.0'
-        }
+    ulr_local_node = 'http://0.0.0.0:8123'
+    base_data = {
+        'id': 1,
+        'jsonrpc': '2.0'
+    }
 
-    def get_account_balance(self, address: str):
-        data = self.base_data
+    def get_account_balance(address: str):
+        data = Handlers.base_data
         data['method'] = 'eth_getBalance'
         data['params'] = [f'{address}', 'latest']
-        response = requests.post(f'{self.ulr_local_node}', data=data)
+        response = requests.post(f'{Handlers.ulr_local_node}', data=data)
         return response.json()['result']
--- routes.py
+++ routes.py
@@ -4,12 +4,10 @@ from handlers import Handlers
 
 private_routes = APIRouter(prefix='/api')
 
-handlers = Handlers('http://0.0.0.0:8123')
-
 
 @private_routes.get('/account.balance')
 async def get_account_balance(token: Annotated[str, Header()], account_address: str):
     if token != '12345':
         raise HTTPException(status_code=400, detail='Неверный токен')
-    balance = handlers.get_account_balance(account_address)
+    balance = Handlers.get_account_balance(account_address)
     return balance
