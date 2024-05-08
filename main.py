68bed9ad98ceafd135a89c0fb5ef86a000d6cb24
Churkabesina <ilyacshutov@yandex.ru>
2024-05-09 02:03:09 +0400

    update

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from fastapi import FastAPI
 from routes import private_routes, token_route
 from db.core import __create_db
 
+tags_metadata = [
+    {
+        'name': 'Auth',
+    },
+    {
+        'name': '1.Account'
+    },
+    {
+        'name': '2.Buying a game'
+    },
+    {
+        'name': '3.WinNFT'
+    },
+    {
+        'name': '4.Vending machine'
+    },
+    {
+        'name': 'AllEndpoints'
+    }
+]
+
 app = FastAPI(title='Validium-test-net.API')
 
 app.include_router(private_routes)
